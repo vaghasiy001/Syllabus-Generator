74bf4292dc4bb3585bd72f7d7e904168bbf739bb
wangel12 <wangeltmg@gmail.com>
2015-11-29 13:36:32 -0500

    DB name change
    I changed my database name to “syllabusgenerator” and pass is blank

--- faculty/includes/connect.php
+++ faculty/includes/connect.php
@@ -3,10 +3,11 @@
 //define("DB_SERVER", "localhost");
 //define("DB_USER", "root");
 //define("DB_PASS", "root");
+//echo "ehlllo";
 define("DB_SERVER", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "root");
-define("DB_NAME", "sg");
+define("DB_PASS", "");
+define("DB_NAME", "syllabusgenerator");
 // 1. Create a database connection
 global $connection;
 $connection = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
