8ee13f049ef7a56f53d63a4e3501829fbeadf909
Soham <vaghasiy001@knights.gannon.edu>
2015-11-28 23:34:32 -0500

    Fixed class location, class time and class day not updating once set.

--- faculty/modifycoursedetails.php
+++ faculty/modifycoursedetails.php
@@ -12,8 +12,6 @@
 $_POST["ddlcnm"]=$_GET["csid"];	
 }
 ?>
-<?php
-?>
 <?php	if (!logged_in()) {
 		redirect_to("index.php");
 	}
@@ -154,7 +152,7 @@ if($cnt==0)
 }
 else
 {
-	$sql="update facultyhours set starttime='".$st."',endtime='".$en."',cday='".$str."' where csid=".$_POST["ddlcnm"]." and uid=".$_SESSION["userid"]." and csid=".$_SESSION["ddlsem3"];
+	$sql="update facultyhours set starttime='".$st."',endtime='".$en."',cday='".$str."',roomno='".$_POST["txtcloc"]."' where csid=".$_POST["ddlcnm"]." and uid=".$_SESSION["userid"]." and semid=".$_SESSION["ddlsem3"];
 	ExecuteNonQuery($sql);
 }
 $_SESSION["mapcsid"]=$_POST["ddlcnm"];
